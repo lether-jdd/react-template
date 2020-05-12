80a2c361a6f57218042046810e431308000d9eb9
jiangdongdong02 <jiangdongdong02@meituan.com>
2020-05-12 18:59:55 +0800

    init

---
+++ config/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      titel: 'react app',
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+};
